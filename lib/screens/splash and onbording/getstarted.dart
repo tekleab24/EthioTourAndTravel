9150220f6b23ef1d75ae158afb1dcc8bab0dcc34
tekleab24 <tekleabmulugeta352@gmail.com>
2023-02-03 21:21:30 +0300

    onboarding page is mestekakeld

--- lib/screens/splash and onbording/getstarted.dart
+++ lib/screens/splash and onbording/getstarted.dart
@@ -18,111 +18,56 @@ class Getstarted extends StatelessWidget {
               colors: [Constants.Onbording_bacground_color, Colors.white],
               begin: Alignment.topCenter)),
       child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: Get.height / 1.7,
-                width: Get.width,
-                margin: const EdgeInsets.all(10),
-                child: Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  padding: const EdgeInsets.all(10),
-                  child: CachedNetworkImage(
-                    fit: BoxFit.cover,
-                    height: 250,
-                    imageUrl:
-                        Get.find<OnbordingController>().onbordlist[1].image_url,
-                    placeholder: (context, url) =>
-                        FaIcon(FontAwesomeIcons.image),
+        // backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: Get.height / 1.7,
+              width: Get.width,
+              margin: const EdgeInsets.all(10),
+              child: Container(child: Image.asset("assets/onboarding1.jpg")),
+            ),
+            SizedBox(
+              child: Column(
+                children: [
+                  const Text(
+                    "Lets Get Started",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
                   ),
-                ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: Get.width,
+                    margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(40)),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              padding: const EdgeInsets.all(15),
+                              elevation: 10,
+                              backgroundColor: Constants.PRIM_COLOR),
+                          onPressed: () {
+                            Get.toNamed("/signup");
+                          },
+                          child: const Text("Sign Up")),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  InkWell(
+                      onTap: () => Get.offNamed("/login"),
+                      child: const Text("Aleardy have an account? Login ")),
+                  const SizedBox(
+                    height: 10,
+                  )
+                ],
               ),
-              SizedBox(
-                child: Column(
-                  children: [
-                    const Text(
-                      "Lets Get Started",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        TextButton(
-                            style: TextButton.styleFrom(
-                                elevation: 10,
-                                padding:
-                                    const EdgeInsets.only(left: 40, right: 40),
-                                backgroundColor: Colors.blue),
-                            onPressed: () {},
-                            child: const FaIcon(
-                              FontAwesomeIcons.facebook,
-                              color: Colors.white,
-                            )),
-                        TextButton(
-                            style: TextButton.styleFrom(
-                                elevation: 10,
-                                padding:
-                                    const EdgeInsets.only(left: 40, right: 40),
-                                backgroundColor: Colors.white),
-                            onPressed: () {},
-                            child: const FaIcon(
-                              FontAwesomeIcons.google,
-                              color: Colors.blue,
-                            )),
-                        TextButton(
-                            style: TextButton.styleFrom(
-                                elevation: 10,
-                                padding:
-                                    const EdgeInsets.only(left: 40, right: 40),
-                                backgroundColor: Colors.white),
-                            onPressed: () {},
-                            child: const FaIcon(
-                              FontAwesomeIcons.apple,
-                              color: Colors.black,
-                            ))
-                      ],
-                    ),
-                    Container(
-                      width: Get.width,
-                      margin:
-                          const EdgeInsets.only(left: 20, right: 20, top: 20),
-                      child: ClipRRect(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(40)),
-                        child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.all(15),
-                                elevation: 10,
-                                backgroundColor: Constants.PRIM_COLOR),
-                            onPressed: () {
-                              Get.toNamed("/signup");
-                            },
-                            child: const Text("Sign Up")),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    InkWell(
-                        onTap: () => Get.offNamed("/login"),
-                        child: const Text("Aleardy have an account? Login ")),
-                    const SizedBox(
-                      height: 10,
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
--- lib/screens/splash and onbording/onbording.dart
+++ lib/screens/splash and onbording/onbording.dart
@@ -14,90 +14,78 @@ class Onbording extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: Get.width,
-      height: Get.height,
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Constants.Onbording_bacground_color, Colors.white],
-              begin: Alignment.topCenter)),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: GetBuilder<OnbordingController>(
-                  init: Get.find<OnbordingController>(),
-                  initState: (_) {},
-                  builder: (onbordcontroller) {
-                    return PageView(
-                        physics: const NeverScrollableScrollPhysics(),
-                        controller: controller,
-                        children: [
-                          onbordscreen(
-                              Get.find<OnbordingController>().onbordlist[0]),
-                          onbordscreen(
-                              Get.find<OnbordingController>().onbordlist[1]),
-                          onbordscreen(
-                              Get.find<OnbordingController>().onbordlist[2]),
-                        ],
-                        onPageChanged: (value) {
-                          onbordcontroller.intialpage.value = value;
-                        });
-                  },
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(14),
-                width: Get.width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Obx(() => Row(
-                          children: List.generate(
-                              3,
-                              (index) => AnimatedContainer(
-                                    duration: const Duration(seconds: 1),
-                                    margin: const EdgeInsets.all(5),
-                                    width: Get.find<OnbordingController>()
-                                                .intialpage
-                                                .value ==
-                                            index
-                                        ? 25
-                                        : 5,
-                                    height: 8,
-                                    decoration: BoxDecoration(
-                                        color: Constants.PRIM_COLOR,
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                  )),
-                        )),
-                    FloatingActionButton(
-                        elevation: 10,
-                        child: FaIcon(
-                          FontAwesomeIcons.arrowRight,
-                          color: Colors.white,
-                        ),
-                        backgroundColor: Constants.PRIM_COLOR,
-                        onPressed: () {
-                          var page =
-                              Get.find<OnbordingController>().intialpage.value;
-                          if (Get.find<OnbordingController>()
-                                  .intialpage
-                                  .value ==
-                              2) {
-                            Get.offNamed("/getstarted");
-                          } else {
-                            controller.jumpToPage(page + 1);
-                          }
-                        })
-                  ],
-                ),
-              )
-            ],
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          Expanded(
+            child: GetBuilder<OnbordingController>(
+              init: Get.find<OnbordingController>(),
+              initState: (_) {},
+              builder: (onbordcontroller) {
+                return PageView(
+                    physics: const NeverScrollableScrollPhysics(),
+                    controller: controller,
+                    children: [
+                      onbordscreen(
+                          Get.find<OnbordingController>().onbordlist[0]),
+                      onbordscreen(
+                          Get.find<OnbordingController>().onbordlist[1]),
+                      onbordscreen(
+                          Get.find<OnbordingController>().onbordlist[2]),
+                    ],
+                    onPageChanged: (value) {
+                      onbordcontroller.intialpage.value = value;
+                    });
+              },
+            ),
           ),
-        ),
+          Container(
+            margin: const EdgeInsets.all(14),
+            width: Get.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Obx(() => Row(
+                      children: List.generate(
+                          3,
+                          (index) => AnimatedContainer(
+                                duration: const Duration(milliseconds: 500),
+                                margin: const EdgeInsets.all(5),
+                                width: Get.find<OnbordingController>()
+                                            .intialpage
+                                            .value ==
+                                        index
+                                    ? 30
+                                    : 8,
+                                height: 8,
+                                decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                    borderRadius: BorderRadius.circular(10)),
+                              )),
+                    )),
+                FloatingActionButton(
+                    elevation: 10,
+                    child: FaIcon(
+                      FontAwesomeIcons.arrowRight,
+                      color: Colors.white,
+                    ),
+                    backgroundColor: Constants.PRIM_COLOR,
+                    onPressed: () {
+                      var page =
+                          Get.find<OnbordingController>().intialpage.value;
+                      if (Get.find<OnbordingController>().intialpage.value ==
+                          2) {
+                        Get.offNamed("/getstarted");
+                      } else {
+                        controller.jumpToPage(page + 1);
+                      }
+                    })
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -106,35 +94,59 @@ class Onbording extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-            image: AssetImage(onbordModel.image_url), fit: BoxFit.cover),
+            image: AssetImage(onbordModel.image_url),
+            fit: BoxFit.cover,
+            colorFilter:
+                const ColorFilter.mode(Colors.black45, BlendMode.luminosity)),
       ),
-      // margin: const EdgeInsets.only(top: 35, left: 15, right: 15),
-      height: Get.height,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            onbordModel.title,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Text(onbordModel.desc),
-          const SizedBox(height: 100),
-          // Center(
-          //   child: CachedNetworkImage(
-          //     fit: BoxFit.fill,
-          //     imageUrl: onbordModel.image_url.toString(),
-          //     placeholder: (context, url) => const Center(
-          //       child: FaIcon(
-          //         FontAwesomeIcons.image,
-          //         color: Constants.PRIM_COLOR,
-          //       ),
-          //     ),
-          //   ),
-          // )
-        ],
+      //  height: Get.height,
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // ignore: prefer_const_constructors
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: const Text(
+                "Ethio T&T",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(14),
+                    child: Text(
+                      onbordModel.title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(14, 0, 14, 100),
+                    child: Text(
+                      onbordModel.desc,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 243, 239, 239),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
